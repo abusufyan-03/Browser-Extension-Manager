15ae9c24114980f961a02a1de30a47b064adb5c7
sufiyan <sufiyan.manuu@gmail.com>
2025-12-05 00:43:18 +0530

    feat: render extension cards dynamically from data.json

---
+++ index.js
@@ -0,0 +1,27 @@
+import data from "./data.json" with {type: "json"};
+const extensionList = document.querySelector('.extension-list');
+let html = '';
+data.forEach((element) => {
+    html += `
+    <div class="extension-card">
+            <div class="extension-data">
+              <img src="${element.logo}">
+              <div class="extension-info">
+                <h3>${element.name}</h3>
+                <p class="extension-description">
+                  ${element.description}
+                </p>
+              </div>
+            </div>
+
+            <div class="extension-control">
+              <button>Remove</button>
+              <div class="toggleUi">
+                <input type="checkbox" id="checkboxInput1">
+                <label for="checkboxInput1" class="toggleSwitch"></label>
+              </div>
+            </div>
+          </div>
+    `;
+    extensionList.innerHTML = html;
+})
